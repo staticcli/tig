e0cdf53a7a72daf51c869a1363d798bd4b55bc3d
staticcli <mjones19867@gmail.com>
2022-06-18 19:04:17 -0400

    Add files via upload

---
+++ p1.ps1
@@ -0,0 +1,116 @@
+Start-Sleep -s 60
+#Compare services after restart
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services |Where-Object {$_.name -eq $obj.name}
+    if($match){write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."}
+}
+#Show services and attempt to start downed services
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services | Where-Object {$_.name -eq $obj.name}
+    if($match){write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."; net start $obj.name}
+}
+#Show only downed services
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services | Where-Object {$_.name -eq $obj.name}
+    if($match){}#write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."}
+}
+#Show services and attempt to start downed services
+#Compare services after restart
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services |Where-Object {$_.name -eq $obj.name}
+    if($match){write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."}
+}
+#Show services and attempt to start downed services
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services | Where-Object {$_.name -eq $obj.name}
+    if($match){write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."; net start $obj.name}
+}
+#Show only downed services
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services | Where-Object {$_.name -eq $obj.name}
+    if($match){}#write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."}
+}
+#Show services and attempt to start downed services
+#Compare services after restart
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services |Where-Object {$_.name -eq $obj.name}
+    if($match){write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."}
+}
+#Show services and attempt to start downed services
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services | Where-Object {$_.name -eq $obj.name}
+    if($match){write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."; net start $obj.name}
+}
+#Show only downed services
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services | Where-Object {$_.name -eq $obj.name}
+    if($match){}#write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."}
+}
+#Show services and attempt to start downed services
+#Compare services after restart
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services |Where-Object {$_.name -eq $obj.name}
+    if($match){write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."}
+}
+#Show services and attempt to start downed services
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services | Where-Object {$_.name -eq $obj.name}
+    if($match){write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."; net start $obj.name}
+}
+#Show only downed services
+$oldServices = Import-Csv -Path "C:\Temp\Service*" | Select-Object name
+$services = Get-Service | Where-Object {$_.Status -eq "Running"}| Select-Object name
+ForEach ($obj in $oldServices)
+{
+    $match = $services | Where-Object {$_.name -eq $obj.name}
+    if($match){}#write-host -ForegroundColor Green $obj.name" is up." }
+    else {write-host -ForegroundColor Red $obj.name" is not up."}
+}
+#Show services and attempt to start downed services
+schtasks.exe /delete /f /tn HRT
+Remove-Item C:\temp\ps.ps1
+#Reboot Start up Completed
---
+++ p2.ps1
@@ -0,0 +1,2 @@
+#Save services prior to restart
+$timestamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }; new-item -Type directory -Path C:\temp;new-item -Type directory -Path C:\archive; move-Item -path C:\temp\serviceslog* -destination C:\archive; get-service | Where-Object {$_.Status -eq "Running"} |Select-object Name, DisplayName| Export-Csv -Path C:\temp\ServicesLog$timestamp.csv; get-content C:\temp\ServicesLog$timestamp.csv
